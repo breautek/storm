20b2df88fc1880b42622a3221d4044a062ffb4c7
Norman Breau <norman@normanbreau.com>
2017-03-12 14:12:22 -0300

    jwt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 module.exports = {
 	Application : require('./lib/Application'),
+	jwt : require('./lib/jwt'),
 	http : require('./lib/http'),
 	db : require('./lib/db'),
 	utils : require('./lib/utils')
