dea1a33fc9e87ff69da1d8bf7c06da212e9544c1
Norman Breau <norman@normanbreau.com>
2019-09-30 23:49:13 -0300

    database unit test

---
+++ spec/database/Database.spec.ts
@@ -0,0 +1,118 @@
+
+import {MockDB} from '../support/MockDB';
+import {
+    MockApplication
+} from '../support/TestApplication';
+
+const MASTER: string = 'MASTER';
+
+describe('Database', () => {
+    let app: MockApplication = null;
+
+    let setup = (done: any) => {
+        app = new MockApplication();
+        app.on('ready', () => {
+            done();
+        });
+    };
+
+    let deconstruct = (done: any) => {
+        app.close().then(() => {
+            app = null;
+            done();
+        });
+    };
+
+    beforeAll(setup);
+    afterAll(deconstruct);
+
+    it('Can add master', () => {
+        let db: MockDB = new MockDB();
+
+        let spy: jasmine.Spy = spyOn(<any>db, '_addNode');
+
+        db.addMaster({test:true});
+
+        expect(spy).toHaveBeenCalledWith(MASTER, {test:true});
+    });
+
+    it('Can only have 1 master', () => {
+        let db: MockDB = new MockDB();
+
+        spyOn(<any>db, '_addNode');
+        db.addMaster({test:true});
+
+        expect(() => {
+            db.addMaster({test:true})
+        }).toThrowError(`Node "${MASTER}" already exists.`);
+    });
+
+    it('Can remove master', () => {
+        let db: MockDB = new MockDB();
+
+        spyOn(<any>db, '_addNode');
+        let spy: jasmine.Spy = spyOn(<any>db, '_removeNode');
+
+        db.addMaster({test:true});
+        db.removeMaster();
+
+        expect(spy).toHaveBeenCalledWith(MASTER);
+    });
+
+    it('Can add slave', () => {
+        let db: MockDB = new MockDB();
+
+        let spy: jasmine.Spy = spyOn(<any>db, '_addNode');
+        let id: string = db.addSlave('test', {test:true});
+        
+        expect(id.indexOf('test')).toBeGreaterThan(-1);
+        expect(spy).toHaveBeenCalledWith(id, {test:true});
+    });
+
+    it('Can remove slave', () => {
+        let db: MockDB = new MockDB();
+
+        let spy: jasmine.Spy = spyOn(<any>db, '_removeNode');
+        spyOn(<any>db, '_addNode');
+        let id: string = db.addSlave('test', {test:true});
+        db.removeSlave(id);
+        
+        expect(spy).toHaveBeenCalledWith(id);
+    });
+
+    it('Canot remove an unexisting slave', () => {
+        let db: MockDB = new MockDB();
+
+        let spy: jasmine.Spy = spyOn(<any>db, '_removeNode');
+        db.removeSlave('test');
+        
+        expect(spy).not.toHaveBeenCalled();
+    });
+
+    it('Can get read connection', () => {
+        let db: MockDB = new MockDB();
+
+        let spy: jasmine.Spy = spyOn(<any>db, '_getConnection');
+        db.getConnection();
+        
+        expect(spy).toHaveBeenCalledWith('SLAVE*', false);
+    });
+
+    it('Can get connection via custom query', () => {
+        let db: MockDB = new MockDB();
+
+        let spy: jasmine.Spy = spyOn(<any>db, '_getConnection');
+        db.getConnection(false, 'test');
+        
+        expect(spy).toHaveBeenCalledWith('test', false);
+    });
+
+    it('can get write connection', () => {
+        let db: MockDB = new MockDB();
+
+        let spy: jasmine.Spy = spyOn(<any>db, '_getConnection');
+        db.getConnection(true);
+        
+        expect(spy).toHaveBeenCalledWith(MASTER, true);
+    });
+});
